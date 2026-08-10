307f3041c55f45d164022a66c515deb3892156f3
Lakshmi Prasanna Sai Kumar <lakshmiprasannasaikumarkambala@gmail.com>
2026-08-10 11:59:15 +0530

    Add degree.java

---
+++ Oops/degree.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class degree{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        List<List<Integer>> l=new ArrayList<>();
+        for(int i=0;i<=n;i++){
+            l.add(new ArrayList<>());
+        }
+        for(int i=0;i<m;i++){
+            int u=sc.nextInt();
+            int v=sc.nextInt();
+            l.get(u).add(v);
+            l.get(v).add(u);
+        }
+        int arr[]=new int[n];
+        for(int i=1;i<=n;i++){
+            int c=0;
+            for(int a:l.get(i)){
+                    c++;
+
+            }
+            arr[i]=c;
+        }
+          System.out.print(Arrays.toString(arr));  
+    }
+}
