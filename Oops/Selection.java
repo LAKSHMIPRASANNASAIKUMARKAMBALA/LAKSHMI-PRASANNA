ed077f2d489e964fc58918d43437ecc6eb94aa36
Lakshmi Prasanna Sai Kumar Kambala <lakshmiprasannasaikumarkambala@gmail.com>
2026-08-14 10:39:22 +0530

    Add Selection.java

---
+++ Oops/Selection.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Selection {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+        for(int i=0;i<n;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+
+        }
+        System.out.print(Arrays.toString(arr));
+    }
+}
